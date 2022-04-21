6d11114f254ce257edfaa001b89ecf40bd7c9b5f
brett hockridge <bretthockridge98@gmail.com>
2022-04-20 20:15:53 -0700

    added buttons to play page and started JavaScript

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var question = document.querySelector('#question');
+
+var answersDiv = document.querySelector("#answers");
+
+var correctAnswer = "correct";
+
+var wrongAnswer = "wrong";
+
+
+var question = document.querySelector(".question")
+
+function clickQuestion() {
+    if (correctAnswer ===  ) {
+        
+    } else {
+        
+    }
+}
